9c882a94fba5dc8726eccf21267999f9113f03e3
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-04-02 13:35:09 -0300

    add: Cancelamento de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,6 @@
 const Appointment = require('../models/Appointment')
 const Yup = require('yup')
-const { startOfHour, parseISO, isBefore, format } = require('date-fns')
+const { startOfHour, parseISO, isBefore, format, subHours } = require('date-fns')
 const { pt } = require('date-fns/locale/pt')
 const User = require('../models/User')
 const File = require('../models/File')
@@ -96,6 +96,30 @@ class AppointmentController {
     })
 
     return res.json(appointment)
+  }
+
+  async delete (req, res) {
+    const appointment = await Appointment.findByPk(req.params.id)
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({ error: "You don't have permission to cancel this appointment." })
+    }
+
+    if (appointment.canceled_at !== null) {
+      return res.status(401).json({ error: 'Appointment already cancelled.' })
+    }
+
+    const dateWithSub = subHours(appointment.date, 2)
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({ error: 'You can only cancel appointments 2 hours in advance.' })
+    }
+
+    appointment.canceled_at = new Date()
+
+    await appointment.save()
+
+    res.json(appointment)
   }
 }
 
