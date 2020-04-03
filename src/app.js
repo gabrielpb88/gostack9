12049debce9cf08a0ff2664de8a1beeb7d1a723e
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-04-03 15:43:18 -0300

    add: Tratamento de exceções e Sentry.io

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const Express = require('express')
+require('express-async-errors')
 const routes = require('./routes')
 const path = require('path')
+const Sentry = require('@sentry/node')
+const sentryConfig = require('./config/sentry')
+const Youch = require('youch')
 
 require('./database')
 
@@ -8,17 +12,29 @@ class App {
   constructor () {
     this.server = new Express()
 
+    Sentry.init(sentryConfig)
+
     this.middlewares()
     this.routes()
+    this.exceptionHandler()
   }
 
   middlewares () {
+    this.server.use(Sentry.Handlers.requestHandler())
     this.server.use(Express.json())
     this.server.use('/files', Express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
   }
 
   routes () {
     this.server.use(routes)
+    this.server.use(Sentry.Handlers.errorHandler())
+  }
+
+  exceptionHandler () {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON()
+      return res.status(500).json(errors)
+    })
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dsn: 'https://e68066ec661840c7be3c39a14e78ba5c@sentry.io/5188081'
+}
